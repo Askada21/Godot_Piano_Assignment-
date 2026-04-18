d5245416027d4c3468e7f6bf6511033044fcc7c1
Daria Osypova <142753291+Askada21@users.noreply.github.com>
2026-04-18 22:16:16 +0100

    Connected button to sounds

--- piano.gd
+++ piano.gd
@@ -1,11 +1,100 @@
 extends Node2D
 
+# single note player (or you can spawn many)
+var note_paths = {
+	# white keys
+	"C":  "res://sounds/C.wav",
+	"D":  "res://sounds/D.wav",
+	"E":  "res://sounds/E.wav",
+	"F":  "res://sounds/F.wav",
+	"G":  "res://sounds/G.wav",
+	"A":  "res://sounds/A.wav",
+	"B":  "res://sounds/B.wav",
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+	# black keys
+	"Cs": "res://sounds/Cs.wav", # C#
+	"Ds": "res://sounds/Ds.wav", # D#
+	"Fs": "res://sounds/Fs.wav", # F#
+	"Gs": "res://sounds/Gs.wav", # G#
+	"As": "res://sounds/As.wav"  # A#
+}
+
+# keyboard mapping
+var key_map = {
+	KEY_A: "C",
+	KEY_W: "Cs",
+	KEY_S: "D",
+	KEY_E: "Ds",
+	KEY_D: "E",
+	KEY_F: "F",
+	KEY_T: "Fs",
+	KEY_G: "G",
+	KEY_Y: "Gs",
+	KEY_H: "A",
+	KEY_U: "As",
+	KEY_J: "B"
+}
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
+func play_note(note):
+	var p = AudioStreamPlayer.new()
+	add_child(p)
+	p.stream = load(note_paths[note])
+	p.play()
+	p.finished.connect(p.queue_free)
+
+#handle keyboard from PC/mobile
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode in key_map:
+			play_note(key_map[event.keycode])
+
+#handle touch/click
+func _on_c_pressed() -> void:
+	play_note("C")
+
+func _on_d_pressed() -> void:
+	play_note("D")
+
+
+func _on_e_pressed() -> void:
+	play_note("E")
+	
+
+
+func _on_f_pressed() -> void:
+	play_note("F")
+
+
+func _on_g_pressed() -> void:
+	play_note("G")
+	
+
+
+func _on_a_pressed() -> void:
+	play_note("A")
+
+
+func _on_b_pressed() -> void:
+	play_note("B")
+
+
+func _on_cs_pressed() -> void:
+	play_note("Cs")
+
+
+func _on_ds_pressed() -> void:
+	play_note("Ds")
+
+
+func _on_fs_pressed() -> void:
+	play_note("Fs")
+
+
+func _on_gs_pressed() -> void:
+	play_note("Gs")
+
+
+func _on_as_pressed() -> void:
+	play_note("As")
